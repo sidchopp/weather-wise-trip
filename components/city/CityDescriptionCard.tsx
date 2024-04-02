a7a194b950144c9057ffa10f1251ff7a0c6cd80a
sidchopp <siddchopp@gmail.com>
2024-04-02 17:05:54 -0600

    CityDescriptionCard More button modified,slight changes in padding

--- components/city/CityDescriptionCard.tsx
+++ components/city/CityDescriptionCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { CityData } from "@/types";
 
 interface CityCardProps {
@@ -8,21 +9,23 @@ const CityDescriptionCard = ({
   cityData,
 }: CityCardProps): React.ReactElement => {
   return (
-    <div className="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-6 flex flex-col h-full">
+    <div className="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-5 flex flex-col h-full">
       <h2 className="text-xl md:text-2xl font-bold text-blue-600 dark:text-blue-500">
         {cityData?.title}
       </h2>
       <p className="text-base lg:text-lg text-gray-500 dark:text-gray-400 py-1">
         {cityData?.description}
       </p>
-      <p className="py-5 flex-grow">{cityData?.extract}</p>
-      <a
-        href={cityData?.content_urls?.desktop?.page}
-        target="_blank"
-        className="max-w-24 inline-flex items-center justify-center py-2 text-base font-medium text-center dark:text-gray-200 bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-700"
-      >
-        More
-      </a>
+      <p className="flex-grow">
+        {cityData?.extract}{" "}
+        <Link
+          href={cityData?.content_urls?.desktop?.page}
+          target="_blank"
+          className="text-left text-base font-medium text-blue-600 dark:text-blue-500 underline underline-offset-4"
+        >
+          More
+        </Link>
+      </p>
     </div>
   );
 };
