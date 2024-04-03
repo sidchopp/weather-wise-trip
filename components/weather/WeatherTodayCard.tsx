a927cd04d0a6a09310e4ea521496b47f000d2d96
sidchopp <siddchopp@gmail.com>
2024-04-03 12:47:35 -0600

    Weather today card added with more features like wind, dew, uv, press

--- components/weather/WeatherTodayCard.tsx
+++ components/weather/WeatherTodayCard.tsx
@@ -15,46 +15,65 @@ const WeatherTodayCard = ({
   return (
     <div className="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg sm:p-4 p-6 flex flex-col h-full">
       <div className="grid grid-cols-2 gap-4 items-center">
-        <h2 className="mb-4 text-5xl md:text-6xl lg:text-7xl font-bold lg:font-extrabold text-gray-900 dark:text-white">
+        <h2 className=" text-5xl md:text-6xl lg:text-7xl font-bold lg:font-extrabold text-gray-900 dark:text-white">
           {Math.round(weatherData.current.temp)}
           <span className="text-blue-600 dark:text-blue-500">°C</span>
         </h2>
-        <div className="flex justify-center">
-          <Image
-            src={`${process.env.WEATHER_ICONS_URL}/${iconCode}@2x.png`}
-            width={80}
-            height={80}
-            alt="Weather icon"
+        <div className="flex justify-end">
+          <SunriseAndSunSet
+            sunrise={weatherData.current.sunrise}
+            sunset={weatherData.current.sunset}
+            timeZone={weatherData.timezone}
           />
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4 items-center text-sm md:text-lg">
+      <Image
+        src={`${process.env.WEATHER_ICONS_URL}/${iconCode}@2x.png`}
+        width={65}
+        height={65}
+        alt="Weather icon"
+      />
+      <div className="grid grid-cols-2 gap-4 items-center text-xs md:text-sm text-slate-700 dark:text-slate-300 font-medium">
         <h3 className="lg:col-span-1">
-          Feels like {Math.round(weatherData.current.feels_like)}°C
+          Feels like:{" "}
+          <span className="font-bold">
+            {Math.round(weatherData.current.feels_like)}°C
+          </span>
         </h3>
-        <p className="lg:col-span-1 flex justify-center">
+        <p className="lg:col-span-1 flex justify-end font-bold">
           {Math.round(weatherToday.temp.max)}°C /{" "}
           {Math.round(weatherToday.temp.min)}°C
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-4 items-center text-xs md:text-sm text-slate-700 dark:text-slate-300 font-medium">
-        <p>
-          Visibility: {Math.round(weatherData.current.visibility / 1000)} miles
-        </p>
-        <p className="flex justify-center">
-          Humidity: {Math.round(weatherData.current.humidity)}%
-        </p>
+      <div className="gap-4 items-center text-xs md:text-sm text-slate-700 dark:text-slate-300 font-medium">
+        <div className="grid grid-cols-2">
+          <p>
+            Visibility: {Math.round(weatherData.current.visibility / 1000)}{" "}
+            miles
+          </p>
+          <p className="flex justify-end">
+            UV Index: {Math.round(weatherData.current.uvi)}
+          </p>
+        </div>
+        <div className="grid grid-cols-2">
+          <p>
+            Pressure: {(weatherData.current.pressure / 1000).toFixed(1)} atm
+          </p>
+          <p className="flex justify-end">
+            Dew point: {Math.round(weatherData.current.dew_point)}°
+          </p>
+        </div>
+        <div className="grid grid-cols-2">
+          <p>Humidity: {Math.round(weatherData.current.humidity)}%</p>
+          <p className="flex justify-end">
+            Wind: {Math.round(weatherData.current.wind_speed)} km/hr
+          </p>
+        </div>
       </div>
-      <p className="text-sm md:text-base lg:text-lg text-gray-700 dark:text-gray-300 py-1">
+
+      <p className="text-sm md:text-base lg:text-lg pt-1 font-medium">
         {weatherToday.summary}
       </p>
-      <div className="mt-auto">
-        <SunriseAndSunSet
-          sunrise={weatherData.current.sunrise}
-          sunset={weatherData.current.sunset}
-          timeZone={weatherData.timezone}
-        />
-      </div>
     </div>
   );
 };
