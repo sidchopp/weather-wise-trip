0308df7fe0648df9377346243f3118200d7ef799
sidchopp <siddchopp@gmail.com>
2024-04-04 17:38:54 -0600

    types for hourly weather added, slight change in tailwind claa name

--- components/weather/WeatherAlerts.tsx
+++ components/weather/WeatherAlerts.tsx
@@ -39,7 +39,7 @@ const WeatherAlerts = ({ weatherAlerts }: any) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black/90" />
+            <div className="fixed inset-0 bg-black opacity-90" />
           </Transition.Child>
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
--- types/weather.ts
+++ types/weather.ts
@@ -30,6 +30,22 @@ export interface WeatherData {
     weather: { id: number; main: string; description: string; icon: string }[];
     snow: { "1h": number };
   };
+  hourly?: {
+    dt: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    dew_point: number;
+    uvi: number;
+    clouds: number;
+    visibility: number;
+    wind_speed: number;
+    wind_deg: number;
+    wind_gust?: number;
+    weather: { id: number; main: string; description: string; icon: string }[];
+    pop: number;
+  }[];
   daily: {
     dt: number;
     sunrise: number;
